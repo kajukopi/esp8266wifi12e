2638dbd129cc863be492cc8e63580652b783ce5d
Karim Roy tampubolon <86301908+kajukopi@users.noreply.github.com>
2025-07-12 06:57:42 +0700

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,31 +1,27 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPUpdateServer.h>
-#include <WebSocketsServer.h>
 #include <ArduinoOTA.h>
 #include <Servo.h>
 #include <WiFiClientSecure.h>
 #include <UniversalTelegramBot.h>
-#include <ArduinoJson.h>
 #include "index.h"
 #include "update.h"
 
 const char* ssid = "karimroy";
 const char* password = "09871234";
-const char* botToken = "YOUR_BOT_TOKEN"; // ganti
-String chatId = "YOUR_CHAT_ID";          // ganti
+const char* botToken = "YOUR_BOT_TOKEN";  // Ganti
+String chatId = "YOUR_CHAT_ID";           // Ganti
+
+ESP8266WebServer server(80);
+ESP8266HTTPUpdateServer httpUpdater;
+Servo myServo;
+const int servoPin = D5;
+const int ledPin = LED_BUILTIN;
 
 WiFiClientSecure secured_client;
 UniversalTelegramBot bot(botToken, secured_client);
 unsigned long lastCheck = 0;
-
-ESP8266WebServer server(80);
-ESP8266HTTPUpdateServer httpUpdater;
-WebSocketsServer webSocket(81);
-
-Servo myServo;
-const int servoPin = D5;
-const int ledPin = LED_BUILTIN;
 
 String getSignalStrength() {
   int rssi = WiFi.RSSI();
@@ -34,31 +30,6 @@ String getSignalStrength() {
   if (rssi > -70) return String(rssi) + " dBm (Good)";
   if (rssi > -80) return String(rssi) + " dBm (Fair)";
   return String(rssi) + " dBm (Weak)";
-}
-
-void webSocketEvent(uint8_t client, WStype_t type, uint8_t *payload, size_t length) {
-  if (type == WStype_TEXT) {
-    String msg = String((char*)payload);
-    DynamicJsonDocument doc(256);
-    if (deserializeJson(doc, msg)) return;
-
-    if (doc.containsKey("servo")) {
-      int val = constrain(doc["servo"].as<int>(), 0, 100);
-      myServo.writeMicroseconds(map(val, 0, 100, 500, 2500));
-    }
-
-    if (doc.containsKey("led")) {
-      digitalWrite(ledPin, (doc["led"] == "on") ? LOW : HIGH);
-    }
-  }
-}
-
-void handleRoot() {
-  server.send(200, "text/html", MAIN_page);
-}
-
-void handleUpdatePage() {
-  server.send(200, "text/html", UPDATE_page);
 }
 
 void handleTelegramBot() {
@@ -75,9 +46,9 @@ void handleTelegramBot() {
           digitalWrite(ledPin, HIGH);
           bot.sendMessage(chatId, "❌ LED dimatikan", "");
         } else if (text.startsWith("/servo_")) {
-          int v = constrain(text.substring(7).toInt(), 0, 100);
-          myServo.writeMicroseconds(map(v, 0, 100, 500, 2500));
-          bot.sendMessage(chatId, "🎚️ Servo ke " + String(v) + "%", "");
+          int val = constrain(text.substring(7).toInt(), 0, 100);
+          myServo.writeMicroseconds(map(val, 0, 100, 500, 2500));
+          bot.sendMessage(chatId, "🎚️ Servo ke " + String(val) + "%", "");
         } else if (text == "/status") {
           String msg = "📡 *ESP8266 Status*\n";
           msg += "🆔 IP: `" + WiFi.localIP().toString() + "`\n";
@@ -94,35 +65,58 @@ void handleTelegramBot() {
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
-  Serial.println("\nConnected: " + WiFi.localIP().toString());
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500); Serial.print(".");
+  }
+  Serial.println("\nConnected! IP: " + WiFi.localIP().toString());
 
   secured_client.setInsecure();
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin, HIGH);  // LED OFF
   myServo.attach(servoPin);
   myServo.writeMicroseconds(500);
 
-  server.on("/", handleRoot);
-  server.on("/update", HTTP_GET, handleUpdatePage);
+  server.on("/", []() {
+    server.send(200, "text/html", MAIN_page);
+  });
+
+  server.on("/update", HTTP_GET, []() {
+    server.send(200, "text/html", UPDATE_page);
+  });
+
   httpUpdater.setup(&server, "/update");
+
+  server.on("/setServo", []() {
+    if (server.hasArg("percent")) {
+      int val = constrain(server.arg("percent").toInt(), 0, 100);
+      myServo.writeMicroseconds(map(val, 0, 100, 500, 2500));
+      server.send(200, "text/plain", "OK");
+    } else {
+      server.send(400, "text/plain", "Missing percent");
+    }
+  });
+
+  server.on("/toggleLED", []() {
+    if (server.hasArg("state")) {
+      String state = server.arg("state");
+      digitalWrite(ledPin, (state == "on") ? LOW : HIGH);
+      server.send(200, "text/plain", "LED " + state);
+    } else {
+      server.send(400, "text/plain", "Missing state");
+    }
+  });
+
+  server.on("/status", HTTP_GET, []() {
+    String json = "{\"ip\":\"" + WiFi.localIP().toString() + "\",\"signal\":\"" + getSignalStrength() + "\"}";
+    server.send(200, "application/json", json);
+  });
 
   server.begin();
   ArduinoOTA.begin();
-  webSocket.begin();
-  webSocket.onEvent(webSocketEvent);
 }
 
 void loop() {
   server.handleClient();
-  webSocket.loop();
   ArduinoOTA.handle();
   handleTelegramBot();
-
-  static unsigned long last = 0;
-  if (millis() - last > 5000) {
-    String json = "{\"ip\":\"" + WiFi.localIP().toString() + "\",\"signal\":\"" + getSignalStrength() + "\"}";
-    webSocket.broadcastTXT(json);
-    last = millis();
-  }
 }
